c8266b83246d8716d8904fb8ed659bc304ff0556
wfkostapavlovski <wfkostapavlovski@192.168.2.135>
2018-07-10 12:08:59 +0200

    proba2

--- Test Suites/TS_RegressionTest.ts
+++ Test Suites/TS_RegressionTest.ts
@@ -12,39 +12,9 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>1286ccc6-2057-418d-87af-23d00cc6cd0e</testSuiteGuid>
    <testCaseLink>
-      <guid>12b1ed24-3d2b-4bed-87e1-7c72470cdce7</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/Main Test Cases/TC1_Verify Successful Login</testCaseId>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>14d98e7b-4ac8-4e28-8ce7-394001d188f0</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>a97344e7-5f9c-4936-92a2-d85e5463f3c9</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>a181a6aa-f677-46db-a4ea-28dad0ccf355</guid>
+      <guid>8f162313-8ebb-405b-96f4-01a036e5c744</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Main Test Cases/TC2_Verify Successful Appointment</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>dd24a84f-9d0a-4e0c-a712-4007eaae747a</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Main Test Cases/Test1</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>3ad2d124-b567-4565-a19f-4348973570fe</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Main Test Cases/Test2</testCaseId>
+      <testCaseId>Test Cases/Common Test Cases/Aga_Test1</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
